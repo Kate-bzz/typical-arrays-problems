0325305c3c4e387c5d32815daa7952f96995f95e
Kate-bzz <73671083+Kate-bzz@users.noreply.github.com>
2021-03-05 23:54:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+  
+  if(typeof array != "undefined" && array != null && array.length > 0){
+    let len = array.length; 
+    var minim = 0;
+    while (len--) {
+      if (array[len] < minim) {
+        minim = array[len];
+      }
+    }
+    return minim;
+  }
+    return 0;
 }
 
 exports.max = function max (array) {
+  if(typeof array != "undefined" && array != null && array.length > 0){
+    let len = array.length;  
+    var maxim = 0;
+    while (len--) {
+      if (array[len] > maxim) {
+        maxim = array[len];
+      }
+    }
+    return maxim;  
+  }
+    return 0;
+ }
+
+exports.avg = function avg (array) {
+  if(typeof array != "undefined" && array != null && array.length > 0){
+  let len = array.length;  
+  var aveg = 0;
+  while (len--) {
+      aveg+= array[len];
+  }
+  aveg/=array.length;
+  return aveg;
+}
   return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
-}
